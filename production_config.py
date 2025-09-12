a23c8d052d583419d4cfed0d06c475fb1bb92627
arubasak <arunava.basak@yahoo.co.in>
2025-09-13 03:21:55 +0530

    Update production_config.py

--- production_config.py
+++ production_config.py
@@ -35,7 +35,7 @@ MAX_RATE_LIMIT_TRACKING = 10000
 MAX_ERROR_HISTORY = 100
 
 # CRM Configuration
-CRM_SAVE_MIN_QUESTIONS = 1  # Minimum questions before CRM save (no longer checks duration)
+CRM_SAVE_MIN_QUESTIONS = 1  # Minimum questions before CRM save
 
 # Evasion Penalties (hours)
 EVASION_BAN_HOURS = [1, 2, 4, 8, 24]
@@ -43,3 +43,7 @@ EVASION_BAN_HOURS = [1, 2, 4, 8, 24]
 # FastAPI Integration
 FASTAPI_EMERGENCY_SAVE_URL = 'https://fifi-beacon-fastapi-121263692901.europe-west4.run.app/emergency-save'
 FASTAPI_EMERGENCY_SAVE_TIMEOUT = 5 # seconds
+
+# Create timedelta objects for easy use
+DAILY_RESET_WINDOW = timedelta(hours=DAILY_RESET_WINDOW_HOURS)
+SESSION_TIMEOUT_DELTA = timedelta(minutes=SESSION_TIMEOUT_MINUTES)
